fb37468afed3b06f5684191c07b8522d418789d7
Joe Miller <josephclaymiller@gmail.com>
2015-08-08 02:11:48 -0700

    Hello World Android
    Enter name and press button for greeting.

---
+++ app/src/main/java/com/josephclaymiller/helloworld/MainActivity.java
@@ -0,0 +1,55 @@
+package com.josephclaymiller.helloworld;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onButtonClick(View v) {
+        // Access text views to access text on the screen
+        TextView textView = (TextView)findViewById(R.id.textView);
+        EditText editText = (EditText)findViewById(R.id.editText);
+        // Extract the name entered in the edit text view
+        String name = editText.getText().toString();
+        // Create a custom greeting using the name
+        String greeting = "Hello " + name;
+        // Change text to show custom greeting
+        textView.setText(greeting);
+    }
+
+}
